84e0da69acdb6bd07dfe83dcd0332298298890be
marcosfavoretti <marcosfavojr@gmail.com>
2025-03-20 11:31:38 -0300

    suporte visual para todas as placas da romi

--- src/app/@core/const/environment.ts
+++ src/app/@core/const/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    API_IP: '192.168.99.102',
-    API_PORT: '3057',
+    API_IP: '192.168.99.129',
+    API_PORT: '9192',
     WS_PORT: '9194',
-    WS_IP: '192.168.99.124'
+    WS_IP: '192.168.99.129'
 }
 // API_IP: '192.168.99.129',
